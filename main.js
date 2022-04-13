6deb79d136bda3e5ea9ca17d33c209e030281f48
Alejandro <aparrilla@elpuig.xeill.net>
2022-04-13 12:48:20 +0200

    mejoras menores

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ fetch("./menu.html")
   .then(data => {
     document.querySelector("menu").innerHTML = data;
   });
+
+  fetch("./footer.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("footer").innerHTML = data;
+    });
