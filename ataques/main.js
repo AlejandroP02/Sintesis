8649d11a006c2998a29c015dfad9720c0a2cecb4
Alejandro <aparrilla@elpuig.xeill.net>
2022-04-27 10:35:03 +0200

    bordes ataques

---
+++ ataques/main.js
@@ -0,0 +1,15 @@
+fetch("./menu.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("menu").innerHTML = data;
+  });
+
+  fetch("./footer.html")
+    .then(response => {
+      return response.text()
+    })
+    .then(data => {
+      document.querySelector("footer").innerHTML = data;
+    });
