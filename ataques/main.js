b428bff62c8e0281a6a4ee91b7228c10b32c7b39
Alex Kevin Muñoz Gil <amunozgil@elpuig.xeill.net>
2022-04-29 10:25:05 +0200

    Java tocado

--- ataques/main.js
+++ ataques/main.js
@@ -13,10 +13,19 @@ fetch("./menu.html")
     .then(data => {
       document.querySelector("footer").innerHTML = data;
     });
-var DDOS = document.getElementById('thorhammer');
+
+var DDOS = document.getElementById('DDOS');
 
 DDOS.addEventListener("click", ataque_DDOS);
 
 function ataque_DDOS() {
-  
+  var Form_DDOS = document.getElementByClass('F-DDOS');
+ if (document.getElementByClass('F-DDoS').classList.contains('hidden')) {
+   Form_DDOS.querySelectorAll( 'div' )
+   .forEach( el => el.classList.remove( 'hidden' ));
+ }
+ else {
+   Form_DDOS.querySelectorAll( 'div' )
+   .forEach( el => el.classList.add( 'hidden' ));
+ }
 }
