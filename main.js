82288b986ecde02a3fb02c2ac4fec379dbdae06f
Alejandro <aparrilla@elpuig.xeill.net>
2022-05-26 17:53:32 +0200

    bibliografia

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ fetch("./menu.html")
     .then(data => {
       document.querySelector("footer").innerHTML = data;
     });
+
+var Bibliografia_B = document.getElementById('bibliografia_B');
+
+Bibliografia_B.addEventListener("click", MOSTRAR_BIBLIOGRAFIA);
+
+function MOSTRAR_BIBLIOGRAFIA() {
+  var Bibliografia_Var = document.getElementById('bibliografia');
+  console.log(Bibliografia_Var.style.display);
+ if (Bibliografia_Var.style.display == 'none') {
+   Bibliografia_Var.style.display = 'block';
+ }
+ else {
+   Bibliografia_Var.style.display = 'none';
+ }
+}
