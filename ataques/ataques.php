8048af76106b8b560579721224119a0793988826
AlejandroP02 <90827205+AlejandroP02@users.noreply.github.com>
2022-06-02 13:36:01 +0200

    aaaaaaaaah

--- ataques/ataques.php
+++ ataques/ataques.php
@@ -167,5 +167,5 @@
 
   </body>
   <script type="text/javascript" src="main.js"></script>
-  <script type="text/javascript" src="atacks.js"></script>
+  <script type="text/javascript" src="attacks.js"></script>
 </html>
